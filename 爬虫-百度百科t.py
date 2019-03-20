68d5f429e2b5d2ac5c83b51a0ae62567c09b8e43
sonichuang <huangshijinsonic@gmail.com>
2019-03-20 20:51:20 +0800

    revise the code

--- 爬虫-百度百科t.py
+++ 爬虫-百度百科t.py
@@ -11,9 +11,9 @@ headers = {agent:agentvalue}
 proxy = {'http':'120.78.174.170:8080'}
 
 response = requests.get(url = url, headers = headers, proxies = proxy)
-code = chardet.detect(response.content)['encoding']#response.content是返回响应的文本数据
+code = chardet.detect(response.content)['encoding']#response.content是返回响应的二进制数据
 response.encoding = code #response.encoding可以查询编码方式，也可以通过赋值对响应进行编码
-html = response.text #response.text返回响应的二进制数据
+html = response.text #response.text返回响应的文本数据
 soup = BeautifulSoup(html, 'html.parser')
 for tag in  soup.find_all(href = re.compile('item')):
     print(tag.text, '-->', ''.join(['http://baike.baidu.com', up.unquote(tag['href'])]))
